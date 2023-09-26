21067970bce016c42700699f8ede69e0cc61a1e1
priscilla <priscillakung@gmail.com>
2023-09-26 15:07:41 -0700

    made article page

---
+++ web/src/pages/ArticlePage/ArticlePage.jsx
@@ -0,0 +1,18 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const ArticlePage = ({ id }) => {
+  return (
+    <>
+      <MetaTags title="Article" description="Article page" />
+
+      <h1>ArticlePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.jsx</code>
+      </p>
+      <p>My article id is {id}</p>
+    </>
+  )
+}
+
+export default ArticlePage
