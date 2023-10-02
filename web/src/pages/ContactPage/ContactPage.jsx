53d331dab19e6ff6f880e434843a287800870a99
priscilla <priscillakung@gmail.com>
2023-10-02 14:00:28 -0700

    add input form validation

--- web/src/pages/ContactPage/ContactPage.jsx
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -4,6 +4,7 @@ import {
   TextField,
   TextAreaField,
   Submit,
+  Label,
 } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
@@ -15,8 +16,10 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
-        <label>Name</label>
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
         <TextField
           name="name"
           validation={{ required: true }}
@@ -24,15 +27,25 @@ const ContactPage = () => {
         />
         <FieldError name="name" className="error" />
 
-        <label>Email</label>
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
         <TextField
           name="email"
-          validation={{ required: true }}
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
           errorClassName="error"
         />
         <FieldError name="email" className="error" />
 
-        <label>Message</label>
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
         <TextAreaField
           name="message"
           validation={{ required: true }}
